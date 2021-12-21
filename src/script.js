31aac6996ab827906d3e1e43c0fa35283c9a8f52
Heng Sok <hengsokhs1333@gmail.com>
2021-12-21 21:05:27 +0700

    Skyfall.

--- src/script.js
+++ src/script.js
@@ -1,3 +1,24 @@
-$(doucment).ready(function () {
-     alert("asdf");
+$(document).ready(function () {
+     /// TESTING 1
+     $("#btn-jquery").on("click", function () {
+          /// REMOVE SELECTED ATTR FROM SELECT TAG
+          $("#sel-pro-lan option").attr("selected", false);
+
+          /// ! CODE
+          /// CUSTOMIZE SELECT OPTION
+          $("#sel-pro-lan option[value='jquery']").attr("selected", true);
+     });
+
+     /// TESTING 2
+     /// BOOTSTRAP BUTTON FUNCTION
+     $("#btn-bootstrap").on("click", function () {
+          let bs = $(this).text().toLowerCase().trim();
+
+          /// REMOVE SELECTED ATTR FROM SELECT TAG
+          $("#sel-pro-lan option").attr("selected", false);
+          
+          /// ! CODE
+          /// CUSTOMIZE SELECT OPTION
+          $("#sel-pro-lan option[value='" + bs +"']").attr("selected", true);
+     });
 });
